98ee745b7978fd3809839fbb69eb6ab93d3fd516
gtjames <gtjames@gmail.com>
2024-08-03 21:23:51 -0500

    updated major1

--- main.c
+++ main.c
@@ -1,173 +1,119 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include "major1.h"
 
-// Function declarations
-int countBits(int num);
+#define INT2BIN(p1) intToBin(p1, malloc(33))
 
 int main() {
-    int choice;
+    int choice, num;
 
     while (1) {
         // Displaying the menu
         printf("\nMenu:\n");
-        printf("1. Option 1: Power\n");
-        printf("2. Option 2: Reverse\n");
-        printf("3. Option 3\n");
-        printf("4. Option 4\n");
-        printf("5. Option 5: Palindrome\n");
-        printf("6. Exit\n");
-        printf("Enter your choice: ");
+        printf("0.  Create a Palindrome\n");
+        printf("1.  Power\n");
+        printf("2.  Reverse\n");
+        printf("3.  Replace\n");
+        printf("4.  Palindrome\n");
+        printf("5.  Exit\n");
         
         // Reading user's choice
-        scanf("%d", &choice);
+        choice = getNumber("Enter your choice: ", 5);
+        num    = getNumber("Enter a number: ", 2000000000);
 
         // Handling user's choice
         switch (choice) {
-            case 1: power();            break;
-            case 2: reverse();          break;
-            case 3: option3();          break;
-            case 4: option4();          break;
-            case 5: palindrome();       break;
-            case 6: printf("Exiting the program.\n");   return 0;
-            default:
-                printf("Invalid choice. Please try again.\n");
+            case 0:     createPalindrome(num);  break;
+            case 1:     power(num);             break;
+            case 2:     reverse(num);           break;
+            case 3:     replace(num);           break;
+            case 4:     palindrome(num);        break;
+            case 5:                             return 0;
+            default:    printf("Invalid choice. Please try again.\n");
         }
     }
     return 0;
 }
 
-// Function definitions
-void power() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    int bitCount = countBits(num);
-}
+int getNumber(char *prompt, int max) {
+    int     num;
+    char    input[30];
 
-int countBits(int num) {
-    int numberOfBits = 0;
-    int firstBit = 0;
-    for (int b = 31; b >= 1; b-- ) {
-        if (num & (1 << b)) {
-            numberOfBits++;
-            if (! firstBit) firstBit = b;
+    while (1) {
+        printf("%s", prompt);
+        fgets(input, sizeof(input), stdin);
+        num = atoi(input);
+        if (num >= 0 && num <= max) {
+            return num;
         }
     }
-    printf("%d %d is %sa power of 2. next hight power of 2 is %.0f", 
-            num, firstBit, numberOfBits>1?"not ":"", pow(2,(firstBit+1)));
-    return numberOfBits;
 }
 
-void reverse() {
-    printf("You selected Option 2.\n");
-    // Add functionality for Option 2 here
+char *intToBin(int v, char *str) {
+    int i;
+    for (i = 31; i >= 0; i--)
+        str[31-i] = ('0' + ((v >> i) & 1));
+    str[32] = 0;
+    return str;
 }
 
-void option3() {
-    printf("You selected Option 3.\n");
-    // Add functionality for Option 3 here
+void createPalindrome(int num) {
+    char    str[33];
+    int     pal = reverse(num) | num;
+    printf("%6d %10u\n       %s\n", num, pal, INT2BIN(pal));
 }
 
-void option4() {
-    printf("You selected Option 4.\n");
-    // Add functionality for Option 4 here
+void power(int num) {
+    int moreThan1 = 0;          //  is there more than one bit?
+    int firstBit  = 0;          //  find the highest order bit
+    for (int b = 31; b >= 1; b-- ) {
+        if (num & (1 << b)) {
+            if (! firstBit) firstBit = b;
+            if( ++moreThan1 > 1)    
+                break;          //  if there are more than 1 bits set it is not a power of 2
+        }
+    }
+    printf("%d is %sa power of 2. next higher power of 2^%d is %.0f\n", 
+            num, moreThan1>1?"not ":"", firstBit, pow(2,(firstBit+1)));
 }
 
-void palindrome() {
-    printf("You selected Option 5.\n");
-    // Add functionality for Option 5 here
+int reverse(int num) {
+    char    str1[32], str2[32];
+    int newNum = 0;
+
+    for (int b = 0; b < 32; b++) {
+        newNum <<= 1;
+        newNum |= ((num & (1 << b)) != 0);
+    }
+    printf("newNum %s\noldNum %s\n", INT2BIN(newNum), INT2BIN(num));
+    return newNum;
 }
 
-/*
-(“Power of 2”, “Reverse Bits”, “Replace Bit Position from Mask”, and “Palindrome”)
-along with a positive integer less than 2 billion, and then perform that operation on that
-integer (based on possible additional input) to produce the result. More details about the
-operations are found below. The twist in this assignment is that each operation must be
-performed using bitwise operators rather than the traditional operations. That said,
-relational operators and the like can be used in branching and loops, but data
-manipulation must be done using bitwise operators.
-This project will be organized in a header file called major1.h, a source code file called
-major1.c, which will contain the main() function, and four individual source code files
-called power.c, reverse.c, replace.c, and palindrome.c that will contain the
-function definition for their respective arithmetic operations. The entire group will be
-responsible in general for non-operation specific functionality in the major1.h and
-major1.c source code files while each individual group member is responsible for
-his/her own “functional operation” in the appropriate files (including the function
-declaration in the header file and the function call inside the main() function).
-In particular, you are expected to have the following functionality for each file:
-• major1.h
-This file is the overall header file for the project and will contain any preprocessor
-directives, such as include and define directives, and function prototypes.
-While the include directives are general for the team, each member is
-expected to add their own function prototype (i.e., function declaration) for the
-operation that he/she is responsible for.
-• major1.c
-This is the code file with the main() function that will do the following: (1) display
-the menu, (2) read in the user’s response for the menu selection, (3) prompt for
-and read in a positive integer less than two billion, and then, based on the menu
-selection, (4) call the appropriate function call for the specified operation, passing
-the integer operand as a parameter to that function. This functionality will be
-contained in a loop that will continue to iterate until the user selects the option to
-end the program. If the user enters a valid outside of the 1 – 5 range, you will
-print a meaningful error message and re-display the menu. Additionally, you will
-continue to prompt for and read in the integer operand until the user enters an
-acceptable value (no error message is needed here). While the code to display
-the menu as well as prompt for and read in the integer operand are considered to
-be part of the group component, each group member is expected to add the
-function call for the operation he/she is responsible for.
-• reverse.c
-This code will contain a single function that accepts a single positive integer less
-than two billion (and the include directive to your header file) to perform the
-following functionality: reverse the bits (all 32 of them) and then print out the
-decimal value of the new integer (i.e., the one with the bits reversed). For
-example, if the user enters the positive integer 2 (which is 00...0010), this
-function would reverse the digits to 0100...00, which is 1073741824. The
-operations to reverse the bits must be done using bitwise operators. One team
-member, and only one team member, will be responsible for the source code in
-this file in GitLab, though collaboration with other group members may be done if
-needed.
-• replace.c
-This code will contain a single function that accepts a single positive integer less
-than two billion (and the include directive to your header file) to perform the
-following functionality: (1) prompt for and read in a positive integer “mask” less
-than three billion and continue to prompt for and read in the positive integer mask
-until the user enters and acceptable value (no error message is needed here), (2)
-prompt for and read in the bit replacement position from the mask and continue
-to prompt for and read in the bit replacement position from the mask until the
-user enters an acceptable value (no error message is needed here, but note that
-since we are working with 32-bit unsigned integers, the value should be between
-0 and 31, inclusively), and finally (3) replace the single bit in the original positive
-integer less than two billion passed to the function with the single bit from the
-positive integer mask specified by the user (i.e., in the bit replacement position
-from the mask). For example, if the user initially enters 7 (in binary, 00...00111)
-as the positive integer less than two billion, then enters 8 (in binary, 00...01000)
-for the positive integer mask and 3 for the bit replacement position, you will
-replace the third bit from the positive integer 7 (a 0) with the third bit from the
-positive integer mask (a 1), resulting in the new value 15 (in binary, 00...01111).
-The operations to replace the bit must be done using bitwise operators.
-Additionally, no loops may be used (except in error checking when prompting for
-and reading in the positive integer mask and bit replacement position from the
-mask) to achieve this functionality. One team member, and only one team
-member, will be responsible for the source code in this file in GitLab, though
-collaboration with other group members may be done if needed.
-• palindrome.c
-This code will contain a single function that accepts a single positive integer less
-than two billion (and the include directive to your header file) to perform the
-following functionality: print out the binary representation of the positive integer
-and then determine if the positive integer is a palindrome (based on all 32-bits).
-For example, if the user enters 1073741826 (in binary, 0100...0010), it is a
-palindrome. Check that 1073741826 is not a palindrome. The operations to
-determine whether or not the positive integer less than two billion is a palindrome
-must be done using bitwise operators. As a hint, you may use an array, but
-remember that the significant logic to determine whether or not the positive
-integer is a palindrome must be done using bitwise operators. One team
-member, and only one team member, will be responsible for the source code in
-this file in GitLab, though collaboration with other group members may be done if
-needed.
-Note that the expectation for this assignment assumes that a group contains 4 students,
-but if, for some reason, a team has only 3 students, then only 3 of the operations would
-need to be supported. This means that each group member is responsible for one and
-only one operation (plus the group component of the assignment). If you have any
-questions on what is acceptable, please discuss this with your instructor.
-SAMPLE OUTPUT (user input shown in bold):
-*/
+void replace(int num) {
+    int mask, bit, maskBit, newNum;
+    bit  = getNumber("Enter a bit:  ", 31);
+    mask = getNumber("Enter a mask: ", 2000000000);
+    bit = 1 << bit;             //  shift the 1 bit to the desired location in the mask
+    maskBit = bit & mask;       //  this will hold the bit to be changed in the position desired
+    if (maskBit == 0) 
+        newNum = num & ~bit;    //  reverse all the bits in the bit so only the masked bit is 0
+    else
+        newNum = num | bit;     //  just OR in the bit in the desired position
+    printf("%s %8d\n%s %08d\n%s %8d\n", INT2BIN(num), num, INT2BIN(bit), num, INT2BIN(newNum), num);
+}
+
+void palindrome(int num) {
+    printf("%d\n%s\nis %sa palindrome\n", num, INT2BIN(num), num == reverse(num) ? "": "not ");
+    //  1073741826      2147581953  2181136448
+    int bPalindrome = 1;
+    printf ("%08x %u\n", num, num);
+    for (int b = 0; b < 16; b++ ) {
+        printf ("%d", (num & (1 << b)) > 0 );
+        if ((num & (1 << b)) != 0 ^ (num & (1 << (31 - b))) != 0) {
+            bPalindrome = 0;
+            break;
+        }
+    }
+    printf("\n%08x %s is %sa palindrome", num, INT2BIN(num), bPalindrome ? "": "not ");
+}
---
+++ major1.h
@@ -0,0 +1,8 @@
+// Function declarations
+void power(int num);
+int  reverse(int num);
+void replace(int num);
+void palindrome(int num);
+int getNumber(char *, int);
+char *intToBin(int v, char *str);
+void createPalindrome(int num);
